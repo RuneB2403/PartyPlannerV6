e015fb28bbf4792e31261bf9a2a86e5db7573300
Rune Bokken <2202239bokken@zuyd.nl>
2023-10-12 09:55:31 +0200

    Kalender update

--- PartyPlannerV6/Controllers/CalendarController.cs
+++ PartyPlannerV6/Controllers/CalendarController.cs
@@ -2,21 +2,30 @@
 using Microsoft.EntityFrameworkCore;
 using PartyPlannerV6.Models;
 using PartyPlannerV6.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PartyPlannerV6.Controllers
 {
     public class CalendarController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public CalendarController( ApplicationDbContext context)
+        public CalendarController(ApplicationDbContext context)
         {
             _context = context;
         }
 
         public IActionResult Calendar()
         {
-            List<Event> events = _context.Events.ToList();
-            return View(events);
+            DateTime currentDate = DateTime.Now;
+            List<Event> pastEvents = _context.Events.Where(e => e.DateTime < currentDate).OrderBy(e => e.DateTime).ToList();
+            List<Event> upcomingEvents = _context.Events.Where(e => e.DateTime >= currentDate).OrderBy(e => e.DateTime).ToList();
+
+            ViewData["PastEvents"] = pastEvents;
+            ViewData["UpcomingEvents"] = upcomingEvents;
+
+            return View();
         }
     }
 }
